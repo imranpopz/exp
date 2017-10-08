ee412db347a74a5048ce3c22bcacbf68c3d1d5f9
SeriniTY320 <SeriniTY320@gmail.com>
2017-10-08 17:25:43 +0300

    HAL: Add sensors HAL

--- lineage_a106.mk
+++ lineage_a106.mk
@@ -146,6 +146,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl
 
+# Sensor HIDL HAL
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.contexthub@1.0-impl
+
 # Messaging
 PRODUCT_PACKAGES += \
 	messaging
