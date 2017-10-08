fdd5b3c8412c7939ab4e0f47b75ce3ed7adb5068
SeriniTY320 <SeriniTY320@gmail.com>
2017-10-08 17:18:56 +0300

    HAL: Add dysp HAL

--- lineage_a106.mk
+++ lineage_a106.mk
@@ -147,6 +147,19 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl
 
+# Display
+PRODUCT_PACKAGES += \
+    libion
+
+# Display HIDL HAL
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.tv.cec@1.0-impl
+
 # Renderscript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
