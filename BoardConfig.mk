debe8dde8138f6f4b6d4b199646b6111473851ee
Manjot Sidhu <manjot.gni@gmail.com>
2017-10-11 10:29:29 +0530

    a106: enable sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,5 +132,5 @@ TARGET_USERIMAGES_USE_EXT4:=true
 USE_CAMERA_STUB := true
 
 # SELinux Policy
-# BOARD_SEPOLICY_DIRS += \
-#    device/micromax/a106/sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/micromax/a106/sepolicy
