1781f5c94eb2c9a7ab8bfd940743aac769ed991c
imranpopz <imranpopz32@gmail.com>
2017-11-16 00:05:15 +0530

    Added H30-U10

--- Android.mk
+++ Android.mk
@@ -25,6 +25,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter a106,$(TARGET_DEVICE)),)
+ifneq ($(filter h30u10,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
--- lineage.mk
+++ lineage.mk
@@ -5,25 +5,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Call device specific makefile
-$(call inherit-product, device/micromax/a106/lineage_a106.mk)
+$(call inherit-product, device/huawei/h30u10/lineage_h30u10.mk)
 
-LOCAL_PATH := device/micromax/a106
+LOCAL_PATH := device/huawei/h30u10
 
 # Common CM stuff
-CM_BUILD := a106
+CM_BUILD := h30u10
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=7.1.2/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="a106-user 7.1.2 MRA58M 2280749 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=7.1.2/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="h30u10-user 7.1.2 MRA58M 2280749 release-keys"
 
-PRODUCT_NAME := lineage_a106
-PRODUCT_DEVICE := a106
-PRODUCT_BRAND := micromax
+PRODUCT_NAME := lineage_h30u10
+PRODUCT_DEVICE := h30u10
+PRODUCT_BRAND := huawei
 PRODUCT_MANUFACTURER := Mediatek
-PRODUCT_MODEL := Micromax Unite 2
+PRODUCT_MODEL := huawei H30-U10
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="a106"
+    PRODUCT_DEVICE="h30u10"
 
 # Correct bootanimation size for the screen
 TARGET_SCREEN_HEIGHT := 800
