e6edd934ec70781c861b336a0b3773d6c07c13e5
imranpopz <imranpopz32@gmail.com>
2018-06-03 17:45:09 +0530

    fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,8 +137,6 @@ SIM_COUNT := 2
 
 BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril/
 
-TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_USERIMAGES_USE_EXT4:=true
 USE_CAMERA_STUB := true
 
